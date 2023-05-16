78b422897d40c986824af523f71a1c52488caa61
koudai4319 <sharbell59@gmail.com>
2023-05-16 21:25:47 +0900

    Active Hashを利用するモデルファイルと商品モデルのアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
 end
