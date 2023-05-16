a43a48d389cc55b62d983dee6a4d89321848df19
koudai4319 <sharbell59@gmail.com>
2023-05-16 22:25:46 +0900

    itemモデルにバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,17 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
 
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 0 }
+    validates :status_id, numericality: { other_than: 0 }
+    validates :shipping_charge_id, numericality: { other_than: 0 }
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :shipping_date_id, numericality: { other_than: 0 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
+
 end
