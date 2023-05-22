d6d0e55d617c90e5a2a997b547f5bb960e079aef
koudai4319 <sharbell59@gmail.com>
2023-05-22 22:16:46 +0900

    商品出品ページの表示、商品の情報をテーブルに保存する処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @items = Item.include(:user)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 end
