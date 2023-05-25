3648960906efd134283346f28886f718b5b7de60
koudai4319 <sharbell59@gmail.com>
2023-05-25 17:59:43 +0900

    itemモデルの金額のバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,7 @@ class Item < ApplicationRecord
     validates :shipping_charge_id, numericality: { other_than: 0 }
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :shipping_date_id, numericality: { other_than: 0 }
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 
 end
